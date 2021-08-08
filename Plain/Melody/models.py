1cabd71d4d85feb7347b5d854b6857b4c682283e
Ktaewon <dydalehdxo@gmail.com>
2021-08-09 01:21:40 +0900

    hashtag update

--- Plain/Melody/models.py
+++ Plain/Melody/models.py
@@ -7,10 +7,13 @@ class Melody(models.Model):
     owner = models.ForeignKey(User, related_name='melody', blank=False, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     body = models.TextField()
-    img = models.ImageField()
+    img = models.ImageField(null=True, upload_to="melody/img/", blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deadline = models.DateTimeField()
     hashtags = TaggableManager()
+    myInstrument = models.TextField()
+    needInstrument = models.TextField(null=True, blank=True)
+    audio = models.FileField(null=False, upload_to="melody/audio/", blank=False)
     def __str__(self):
         return f'[{self.pk}]{self.title}'
--- Plain/Melody/urls.py
+++ Plain/Melody/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.melody),
     path("uploads/", views.upload_melody, name="upload"),
+    path("detail/<str:id>", views.detail, name="detail"),
     #path('tag/', views.TagCloudTV.as_view(), name='tag_cloud'),
     #path('tag/<str:tag>', views.TaggedObjectLV.as_view(), 
     #    name='tagged_object_list'),
