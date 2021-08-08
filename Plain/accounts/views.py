3d82f2ae7bfa322b9e919ee99308d54fbd485bf9
Sangwon <psangwon62@gmail.com>
2021-08-09 04:02:22 +0900

    login

---
+++ Plain/accounts/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+from .models import user
+
+# Create your views here.
+def signup(request):
+    if request.method == "POST":
+        if request.POST['password1'] == request.POST['password2']:
+            user=User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+            auth.login(request, user)
+            return redirect('login')
+    return render(request, 'signup.html')
+
+def create(request):
+    new_user=user()
+    new_user.email=request.POST['email']
+    new_user.nickname=request.POST['nickname']
+    new_user.genre=request.POST['genre']
+    new_user.instrument=request.POST['instrument']
+    new_user.save()
+    return redirect('login', new_user.id)
+
+def login(request):
+    if request.method == "POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        user=auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('signup')
+        else:
+            return render(request, 'login.html', {'error': 'username or password is incorrect'})
+    else:
+        return render(request, 'login.html')
