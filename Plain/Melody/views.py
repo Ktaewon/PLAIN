65f5efb6258e3d0ebc203642a9bf875b5ce0ec2d
이현송 <hslee791@gmail.com>
2021-08-09 06:05:24 +0900

    likes

--- Plain/Melody/views.py
+++ Plain/Melody/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render, get_object_or_404
-from .models import Melody
+from .models import Melody, Comment
 from .forms import MelodyForm
 from django.utils import timezone
 from django.contrib import messages
@@ -10,8 +10,19 @@ def melody(request):
     #return render(request, 'melody_upload.html', {'form' : form })
     return render(request, 'melody_upload.html')
 def detail(request, id):
+    detail = get_object_or_404(Melody, pk=id)
+    comments = Comment.objects.all().filter(Comment_post = detail)
+
     melody = get_object_or_404(Melody, pk=id)
-    return render(request, 'melody_detail.html', {'melody' : melody})
+
+
+    if detail.likes.filter(id=request.user.id):
+        message= "좋아요 취소"
+    else: 
+        message = "좋아요"
+    return render(request, 'melody_detail.html', 
+    {'melody' : melody, 'detail' : detail, 'commnets': comments, 'message':message })
+
 def upload_melody(request):
     if request.method == "POST":
         melody = Melody()
@@ -31,3 +42,41 @@ def upload_melody(request):
         melody.save()
         messages.warning(request, 'Upload Finish!')
         return redirect('detail', melody.id)
+
+
+
+
+
+
+
+# Comments
+def comment(request, melody_id):
+        if request.method == "POST" :
+            comment = Comment()
+            comment.body = request.POST['body']
+            comment.pub_date = timezone.datetime.now()
+            comment.writer = request.user
+            comment.post = get_object_or_404(Melody, pk=melody_id)
+            comment.save()
+
+            return redirect('/melody/'+str(melody_id))
+        else:
+            return redirect('/melody/'+str(melody_id))
+
+def comment_delete(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    melody_id = comment.post.id
+    comment.delete()
+    return redirect('/melody/'+str(melody_id))
+
+# likes
+def post_like(request, melody_id):
+    melody = get_object_or_404(Melody, pk=melody_id)
+    user = request.user
+
+    if melody.likes.filter(id=user.id):
+        melody.likes.remove(user)
+    else: 
+        melody.likes.add(user)
+
+    return redirect('/melody/detail/' + str(melody_id))
