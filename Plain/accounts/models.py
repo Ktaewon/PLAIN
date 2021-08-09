d46b07e524315dde9cc67e5fc1e691e5204b1fa0
이현송 <hslee791@gmail.com>
2021-08-09 16:16:22 +0900

    user

--- Plain/accounts/models.py
+++ Plain/accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.db.models.fields import TextField
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, User
 
 # Create your models here.
 
