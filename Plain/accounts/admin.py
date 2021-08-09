806c13d5af8aa16d03ddefc133bacccfc2337238
Sangwon <psangwon62@gmail.com>
2021-08-09 21:16:23 +0900

    login

--- Plain/accounts/admin.py
+++ Plain/accounts/admin.py
@@ -2,13 +2,11 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
-    
     list_display = (
         'nickname',
         'email',
         'genre',
     )
-
     list_display_links = (
         'nickname',
         'email',
