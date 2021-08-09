4443b2f90a7a8a32dbd7bbc17d4a65058cc301bd
Sangwon <psangwon62@gmail.com>
2021-08-09 15:21:23 +0900

    profile

--- Plain/accounts/models.py
+++ Plain/accounts/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.db.models.fields import TextField
+from django.contrib.auth.models import User
+
 # Create your models here.
 
-class user(models.Model):
-    email=models.CharField(max_length=30)
-    nickname=models.CharField(max_length=20)
-    genre=models.CharField(max_length=20)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email=models.CharField(max_length=30, blank=True)
+    nickname=models.CharField(max_length=20, blank=True)
+    genre=models.CharField(max_length=20, blank=True)
     instrument=models.TextField()
