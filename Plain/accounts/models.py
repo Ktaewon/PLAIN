fc52beb9021d922e5032fbea119d5b532c4ddd2f
Ktaewon <dydalehdxo@gmail.com>
2021-08-12 11:49:14 +0900

    회원가입 최종수정

--- Plain/accounts/models.py
+++ Plain/accounts/models.py
@@ -30,6 +30,7 @@ class User(AbstractBaseUser):
     )
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+    objects = UserManager()
     USERNAME_FIELD = 'email'
     def __str__(self):
         return self.email
