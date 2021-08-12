6f1fe653c625d2569d59993dfa477e70372d2830
Ktaewon <dydalehdxo@gmail.com>
2021-08-12 12:43:46 +0900

    재생 아이콘 클릭 시 변경

--- Plain/Melody/static/js/melody_default.js
+++ Plain/Melody/static/js/melody_default.js
@@ -1,3 +1,15 @@
 $("#subupload-btn").on('click', function() {
     document.all.commendInput.click();
 });
+var check = 0;
+$("#playbutton").on('click', function() {
+    wavesurfer.playPause();
+    if (check == 0){
+        check = 1;
+        document.getElementById("playbutton").classList.replace("fa-play-circle", "fa-pause-circle");
+    }
+    else {
+        check = 0;
+        document.getElementById("playbutton").classList.replace("fa-pause-circle", "fa-play-circle");
+    }
+});
