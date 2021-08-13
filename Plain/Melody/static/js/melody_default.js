f78d1b2ab172f9f2bf016c9a3c052a038d554e38
Ktaewon <dydalehdxo@gmail.com>
2021-08-13 20:05:39 +0900

    icon change fixed

--- Plain/Melody/static/js/melody_default.js
+++ Plain/Melody/static/js/melody_default.js
@@ -14,13 +14,19 @@ $("#playbutton").on('click', function() {
     }
 });
 
-$("#comment_play").on('click', function() {
-    if (check == 0){
-        check = 1;
-        document.getElementById("comment_playbutton").classList.replace("fa-play-circle", "fa-pause-circle");
+
+comment_plays = document.getElementsByClassName("cm-play");
+
+function change_play(event) {
+    button = this.getElementsByClassName("button")[0];
+    if (button.classList.contains("fa-play-circle")) {
+        button.classList.replace("fa-play-circle", "fa-pause-circle");
     }
     else {
-        check = 0;
-        document.getElementById("comment_playbutton").classList.replace("fa-pause-circle", "fa-play-circle");
+        button.classList.replace("fa-pause-circle", "fa-play-circle");
     }
-});
+}
+
+for (i = 0; i < comment_plays.length; i++) {
+    comment_plays[i].addEventListener("click" , change_play);
+}
